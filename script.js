b78fbaa69fa63bd4a05e9b65d3016cab6313706e
kunaalkt <121182233+kunaalkt@users.noreply.github.com>
2024-06-02 20:44:44 +0530

    Updated styling

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-document.getElementById('fetch-posts').addEventListener('click', fetchPosts);
+const fetchButton = document.getElementById("fetch-posts");
+fetchButton.addEventListener("click", fetchPosts);
 
 function fetchPosts() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => {
-            const postsContainer = document.getElementById('posts-container');
-            postsContainer.innerHTML = '';
-            data.forEach(post => {
-                const postElement = document.createElement('div');
-                postElement.classList.add('post');
-                postElement.innerHTML = `
-                    <h2>${post.title}</h2>
-                    <p>${post.body}</p>
-                `;
-                postsContainer.appendChild(postElement);
-            });
-        })
-        .catch(error => console.error('Error fetching posts:', error));
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((data) => {
+      const postsContainer = document.getElementById("posts-container");
+      postsContainer.innerHTML = "";
+      data.forEach((post) => {
+        const postElement = document.createElement("div");
+        postElement.classList.add("post");
+        postElement.innerHTML = `
+          <h2>${post.title}</h2>
+          <span class="user-details" data-id=${post.id}></span>
+          <p>${post.body}</p>
+        `;
+        postsContainer.appendChild(postElement);
+        fetchButton.disabled = true;
+      });
+    })
+    .catch((error) => console.error("Error fetching posts:", error));
 }
